95e52641119cdc5ef922f385b7245b7fea4f939d
zhiren lu <nzlu@ncsu.edu>
2016-10-02 19:03:23 +0000

    add a option that admin can create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the NCSU Library!"
-      redirect_to @user
+      if current_user.nil?
+        log_in @user
+        flash[:success] = "Welcome to the NCSU Library!"
+        redirect_to @user
+      else
+       flash[:success] = "have create the User/Admin"
+       redirect_to @user
+      end
     else
       render 'new'
     end
